9ecd54b59cf0fca8ed701278a6b40b37ab6fc3f2
Meisuleen Kased <meisuleen@gmail.com>
2018-11-10 20:27:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@
  def reverse_each_word(string)
    original_array = string.split(" ")
    new_array = []
-   string.collect do |string|
+   string.map do |string|
      new_array << string.reverse
    end
    new_array.join(" ")
