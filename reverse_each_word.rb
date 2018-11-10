3f08228394b57de97b097a3535cd3920aee6586c
Meisuleen Kased <meisuleen@gmail.com>
2018-11-10 20:12:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 require 'pry'
 def reverse_each_word(string)
-  binding.pry
    original_array = string.split(" ")
-
+   new_array = []
+   original_array.each do |string|
+     new_array << string.reverse
+   end
+  new_array
 end
